13a7ae1a8048e28c6c57b877871612a2ff46bd8c
Raaj-Chimulkar <raajchimulkar.com>
2024-05-19 14:21:31 +0530

    Final

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express, { request, response } from "express";
+import { port, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/booksModel.js";
+import router from "./routes/Routes.js";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+
+app.use(express.json());
+
+app.get('/', (request, response)=>{
+console.log(request);
+return response.status(234).send('Welcome!')
+})
+
+app.use('/books',router);
+
+mongoose.connect(mongoDBURL)
+.then(()=>{
+console.log('App connected to db!')
+app.listen(port,()=>{
+    console.log(`App is running fine on Port : ${port}` );
+});
+})
+.catch((error)=>{
+console.log(error)
+});
